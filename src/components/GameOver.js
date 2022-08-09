b8cc9c3b7626487cc8a6de86f8a6f9cc791509ea
ninapalmadev <ninapalmadev@gmail.com>
2022-08-09 09:31:35 -0300

    Alterna estágios do jogo

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,9 +1,10 @@
 import "./GameOver.css";
 
-const GameOver = () => {
+const GameOver = ({retry}) => {
   return (
     <div>
-
+        <h1>GameOver</h1>
+        <button onClick={retry}>Resetar o jogo</button>
     </div>
   )
 }
